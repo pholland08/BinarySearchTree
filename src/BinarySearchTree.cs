89bbb6c6c833da256cea4548a4af16385c30ff4d
pholland08 <bulldog105@gmail.com>
2017-05-08 13:39:08 -0500

    Added iterative insertion method

--- src/BinarySearchTree.cs
+++ src/BinarySearchTree.cs
@@ -15,39 +15,82 @@ namespace BinarySearchTree
             get { return _Root; }
             set { _Root = value; }
         }
+        private int _Count;
+        public int Count
+        {
+            get { return _Count; }
+        }
         #endregion Fields
 
         #region Constructors
         public BinarySearchTree()
         {
+            _Count = 0;
             Root = null;
         }
         public BinarySearchTree(T info) : this()
         {
-            BinarySearchNode<T> node = new BinarySearchNode<T>(info);
-            Root = node;
-            Root.RightFlag = true;
-            Root.RightChild = Root;
-            Root.LeftChild = Root;
+            Insert(info);
         }
         #endregion Constructors
 
         #region Methods
         // TODO Write method Insert
-        public bool Insert(T obj)
+        public void Insert(T obj)
         {
+            Insert(obj, ref _Root);
+        }
+        public void Insert(T obj, ref BinarySearchNode<T> Root)
+        {
+            _Count += 1;
             BinarySearchNode<T> newnode = new BinarySearchNode<T>(obj);
             // Check if root node is empty
             if (Root == null)
             {
+                newnode.LeftChild = newnode.RightChild = newnode;
+                //newnode.RightFlag = true; // newnode.LeftFlag set to false at instantiation 
                 Root = newnode;
-                Root.RightFlag = true;
-                Root.RightChild = Root;
-                Root.LeftChild = Root;
-                return true;
             }
-            // TODO Add method body
-            return false;
+            else
+            {
+                BinarySearchNode<T> current = Root;
+                bool finished = false;
+                while (!finished)
+                {
+                    if (current.Info.CompareTo(newnode.Info) < 1)
+                    {
+                        if (!current.RightFlag)
+                        {
+                            newnode.RightChild = current.RightChild;
+                            newnode.RightFlag = current.RightFlag;
+                            current.RightChild = newnode;
+                            current.RightFlag = true;
+                            newnode.LeftChild = current; // newnode.LeftFlag set to false at instantiation
+                            finished = true;
+                        }
+                        else
+                        {
+                            current = current.RightChild;
+                        }
+                    }
+                    else
+                    {
+                        if (!current.LeftFlag)
+                        {
+                            newnode.LeftChild = current.LeftChild;
+                            newnode.LeftFlag = current.LeftFlag;
+                            current.LeftChild = newnode;
+                            current.LeftFlag = true;
+                            newnode.RightChild = current; // newnode.RightFlag set to false at instantiation
+                            finished = true;
+                        }
+                        else
+                        {
+                            current = current.LeftChild;
+                        }
+                    }
+                }
+            }
         }
         // TODO Write method FindIterative
         // TODO Write method FindRecursive
