09af9d16db20274ca44930601b867e0cfa194a4a
pholland08 <bulldog105@gmail.com>
2017-05-09 04:55:44 -0500

    Cleanup commit

--- src/BinarySearchTree.cs
+++ src/BinarySearchTree.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
@@ -189,7 +185,5 @@ namespace BinarySearchTree
                 
         #endregion Methods
 
-    }
-
-    // TODO Write class TreeStack for iterative methods
+    }    
 }
