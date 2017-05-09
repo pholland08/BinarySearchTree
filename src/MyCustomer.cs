98191e209fa8a870af2910b34707e1ccd51f0adc
pholland08 <bulldog105@gmail.com>
2017-05-09 04:53:47 -0500

    Cleanup commit

---
+++ src/MyCustomer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class MyCustomer : IComparable<MyCustomer>
+    {
+        public string name;
+        public string phone;
+
+        #region Constructors
+        public MyCustomer()
+        {
+            name = "";
+            phone = "";
+        }
+        public MyCustomer(string name, string phone) : this()
+        {
+            this.name = name;
+            this.phone = phone;
+        }
+        #endregion Constructors
+
+        #region Methods
+        public int CompareTo(MyCustomer obj)
+        {
+            if (obj.name == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return string.Compare(name, obj.name, true);
+            }
+        }
+
+        public static MyCustomer[] BuildMCA(string[,] customers)
+        {
+            MyCustomer[] MCA = new MyCustomer[customers.Length / 2];
+            for (int index = 0; index < customers.Length / 2; index++)
+            {
+                MCA[index] = new MyCustomer(customers[index, 0], customers[index, 1]);
+            }
+            return MCA;
+        }
+        #endregion Methods
+    }
+}
